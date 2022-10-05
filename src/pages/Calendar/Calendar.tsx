61d34fe8c7f3794023d8a0f9273ee403d5fbd5df
hyojinjeong <hyojin.dev@gmail.com>
2022-10-05 23:24:11 +0900

    feat: 달력 이전 달 조회
    - 이전 달 조회 기능 구현

--- src/pages/Calendar/Calendar.tsx
+++ src/pages/Calendar/Calendar.tsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import { DAYS } from "../../constants/date";
 import { CalendarHeader, Days, DatesGrid, Dates } from "./style";
-import { getMonthDate, getTodayMonth } from "../../utils/date"
+import { getTodayYear, getTodayMonth, getMonthDate } from "../../utils/date"
+
+interface MonthDate{
+  year: number,
+  month: number,
+  date: number,
+  day: number
+}
 
 const Calendar = () => {
   let month = getMonthDate(2022, 10);
-  console.log(month);
+  const [curYear, setCurYear] = useState<number>(getTodayYear());
+  const [curMonth, setCurMonth] = useState<number>(getTodayMonth());
+  const [monthDate, setMonthDate] = useState<Array<MonthDate>>(getMonthDate(curYear, curMonth))
+
+  const prevMonth = () => {
+    setCurMonth(prev => --prev)
+  }
+
+  useEffect(()=>{
+    console.log('changed', curMonth);
+    setMonthDate(getMonthDate(curYear, curMonth))
+  },[curYear, curMonth])
+
   return (
     <Layout>
       <CalendarHeader>
@@ -14,12 +34,13 @@ const Calendar = () => {
         ))}
       </CalendarHeader>
       <DatesGrid>
-        {month.map((dates)=>(
+        {monthDate.map((dates)=>(
           <Dates isThisMonth={dates.month === getTodayMonth() ? true : false}>
             {dates.date}
           </Dates>
         ))}
       </DatesGrid>
+      <button onClick={prevMonth}>button</button>
     </Layout>
   );
 };
