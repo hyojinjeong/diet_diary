a916ad133228bf485a07bd15c93500406eb46751
hyojinjeong <hyojin.dev@gmail.com>
2022-10-10 09:23:01 +0900

    style: diet 데이터 조회 결과 컴포넌트 스타일 변경
    - row-gap, font-size 조정

--- src/components/ResultComponent/ResultComponent.tsx
+++ src/components/ResultComponent/ResultComponent.tsx
@@ -33,11 +33,37 @@ const ResultComponent = () => {
             <ContentTitle>
               <span>💧 물</span>
             </ContentTitle>
+            <Box
+              sx={{
+                width: "100%",
+                height: "calc(100% - 1.7rem)",
+                display: "flex",
+                justifyContent: "space-around",
+                alignItems: "center",
+              }}
+            >
+              <DietItem />
+              <DietItem />
+              <DietItem />
+            </Box>
           </Box>
           <Box>
             <ContentTitle>
               <span>🏃🏻‍♂️ 신체</span>
             </ContentTitle>
+            <Box
+              sx={{
+                width: "100%",
+                height: "calc(100% - 1.2rem)",
+                display: "flex",
+                justifyContent: "space-around",
+                alignItems: "center",
+              }}
+            >
+              <DietItem />
+              <DietItem />
+              <DietItem />
+            </Box>
           </Box>
         </ContentGrid>
       </Wrapper>
--- src/components/ResultComponent/style.ts
+++ src/components/ResultComponent/style.ts
@@ -18,19 +18,19 @@ export const ContentGrid = styled.div`
   align-content: space-around;
   justify-content: center;
   padding: 1rem;
-  row-gap: 1rem;
+  row-gap: 0.5rem;
 `;
 
 export const ContentTitle = styled.div`
   width: 100%;
-  font-size: 1.7rem;
   font-weight: bold;
+  font-size: 1.2rem;
 `;
 
 export const DietItem = styled.div`
   width: 30%;
   height: calc(100% - 1rem);
   background-color: red;
-  margin: 0 1rem;
   border-radius: 10px;
+  margin-top: 0.5rem;
 `;
