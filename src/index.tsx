6df1378088deca20bc76e86609752f7725ed97e9
hyojinjeong <hyojin.dev@gmail.com>
2022-10-23 16:29:58 +0900

    style: custom 테마 추가
    - primary main color 변경

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import GlobalStyles from "./styles/GlobalStyles";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#8F83D6",
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
