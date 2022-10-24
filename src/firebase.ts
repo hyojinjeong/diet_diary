12f572177944f70682b4e6f070d18fd068faef3d
hyojinjeong <hyojin.dev@gmail.com>
2022-10-24 10:46:22 +0900

    comment: 주석 처리 삭제
    - App.tsx, firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,14 +13,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
   appId: process.env.REACT_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
-  // apiKey: "AIzaSyAz6ol88v9R6Wk1-j0trRtMJALqMJeYdr4",
-  // authDomain: "diet-diary-e1d0f.firebaseapp.com",
-  // projectId: "diet-diary-e1d0f",
-  // storageBucket: "diet-diary-e1d0f.appspot.com",
-  // messagingSenderId: "711868820347",
-  // appId: "1:711868820347:web:77378d63c439d31d492bde",
-  // measurementId: "G-9TZCDCFQ21"
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -28,4 +21,3 @@ const app = firebase.initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export { app, auth };
-
