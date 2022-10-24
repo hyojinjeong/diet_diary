5675bb33cfd2f209c4eb19ba015f293dd3fafd9e
hyojinjeong <hyojin.dev@gmail.com>
2022-10-24 19:55:24 +0900

    feat: 회원가입 유효성 검사
    - 이메일 형식 검사
    - 이메일, 비밀번호 필수값  체크

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -9,6 +9,7 @@ import CloseIcon from "@mui/icons-material/Close";
 import { useForm } from "react-hook-form";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
+import { ErrorMessage } from "./style";
 
 interface RegisterProps {
   regOpen: boolean;
@@ -21,7 +22,11 @@ type FormValues = {
 };
 
 const Register = ({ regOpen, handleRegOpen }: RegisterProps) => {
-  const { register, handleSubmit } = useForm<FormValues>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
   const onSubmit = handleSubmit(async (data) => {
     const user = await createUserWithEmailAndPassword(
       auth,
@@ -49,25 +54,38 @@ const Register = ({ regOpen, handleRegOpen }: RegisterProps) => {
         </IconButton>
       </DialogTitle>
       <form>
-        <DialogContent dividers sx={{ padding: "15px" }}>
+        <DialogContent dividers sx={{ padding: "15px", maxWidth: "476px" }}>
           <TextField
             id="standard-basic"
             label="Email"
             variant="outlined"
             fullWidth
             sx={{ margin: "10px 0" }}
-            {...register("email")}
+            error={Boolean(errors.email)}
+            {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
           />
+          {errors.email && errors.email.type === "required" && (
+            <ErrorMessage>*필수 입력값입니다.</ErrorMessage>
+          )}
+          {errors.email && errors.email.type === "pattern" && (
+            <ErrorMessage>*메일의 형식이 잘못되었습니다.</ErrorMessage>
+          )}
           <TextField
             id="standard-basic"
             label="Password"
             variant="outlined"
             fullWidth
             type="password"
-            error={false}
+            error={Boolean(errors.password)}
             sx={{ margin: "10px 0" }}
-            {...register("password")}
+            {...register("password", { required: true, minLength: 6 })}
           />
+          {errors.password && errors.password.type === "required" && (
+            <ErrorMessage>*필수 입력값입니다.</ErrorMessage>
+          )}
+          {errors.password && errors.password.type === "minLength" && (
+            <ErrorMessage>*6자리 이상 입력해주세요.</ErrorMessage>
+          )}
         </DialogContent>
         <DialogActions sx={{ padding: "16px 24px" }}>
           <Button variant="contained" onClick={onSubmit}>
--- src/components/Register/style.ts
+++ src/components/Register/style.ts
@@ -1,9 +1,5 @@
 import styled from "@emotion/styled";
 
-export const ContentWrapper = styled.div<{ isOpen: boolean }>`
-  padding-top: 64px;
-  margin-left: ${({ isOpen }) => (isOpen ? "250px" : 0)};
-  min-width: 350px;
-  height: 100vh;
-  transition: margin-left 0.5s;
+export const ErrorMessage = styled.span`
+  color: red;
 `;
